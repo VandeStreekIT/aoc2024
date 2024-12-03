4be210ece85f5c375f252c15666568a234968bbc
Pim van de Streek <git@vandestreek.it>
2024-12-03 16:57:27 +0100

    completed day1 part1

--- src/Day1.java
+++ src/Day1.java
@@ -1,8 +1,29 @@
-import java.io.FileNotFoundException;
-import java.io.File;
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Day1 {
+    private String filePath = "input/day1.txt";
+
     public static void main(String[] args) {
+        Day1 d = new Day1();
+        String content = Util.readFile(d.filePath);
+        String[] lines = content.split("\n");
+
+        int[] array1 = new int[lines.length];
+        int[] array2 = new int[lines.length];
+
+        for (int i = 0; i < lines.length; i++) {
+            String[] values = lines[i].split("   ");
+            array1[i] = Integer.parseInt(values[0]);
+            array2[i] = Integer.parseInt(values[1]);
+        }
+
+        Arrays.sort(array1);
+        Arrays.sort(array2);
+
+        int sum = 0;
+        for (int i = 0; i < array1.length; i++) {
+            sum += Math.abs(array1[i]-array2[i]);
+        }
+        System.out.println(sum);
     }
-}
+    }
--- src/Util.java
+++ src/Util.java
@@ -1,2 +1,16 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+
 public class Util {
+    public static String readFile(String filePath) {
+        String content = null;
+        try {
+            // Lees het bestand als één enkele string
+            content = new String(Files.readAllBytes(Paths.get(filePath)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content;
+    }
 }
