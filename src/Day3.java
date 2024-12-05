fe046f0f046c78daa4a6d7ea9bc291324d57ad1c
Pim van de Streek <git@vandestreek.it>
2024-12-05 13:47:25 +0100

    Stuck on day 3 part 2

--- src/Day3.java
+++ src/Day3.java
@@ -1,9 +1,7 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class Day3 {
     private final String filePath = "input/day3.txt";
@@ -11,22 +9,40 @@ public class Day3 {
     private int part2;
 
     public static void main(String[] args) {
-
         Day3 d = new Day3();
         String memory = Util.readFile(d.filePath);
-
+        boolean enable = true;
         String regex = "mul\\(\\d{1,3},\\d{1,3}\\)";
         Pattern pattern = Pattern.compile(regex);
 
-        Matcher matcher = pattern.matcher(memory);
-        while (matcher.find()) {
-            List<Integer> calculation = Arrays.stream(
-                    matcher.group()
-                            .substring(4, matcher.group().length() - 1)
-                            .split(","))
-                    .map(Integer::valueOf).toList();
-            d.part1 += calculation.get(0) * calculation.get(1);
+        for (int i = 0; i < memory.length(); i++) {
+            if (memory.substring(i, (Math.min(i + 7, memory.length() -1 ))).equals("don't()")) {
+                enable = false;
+                i += 7;
+                continue;
+            }
+            if (memory.substring(i, (Math.min(i + 4, memory.length() -1 ))).equals("do()")) {
+                enable = true;
+                i += 4;
+                continue;
+            }
+            String subSting = memory.substring(i, (Math.min(i + 12, (memory.length() - 1))));
+            if (subSting.matches(regex + ".*")) {
+                Matcher matcher = pattern.matcher(subSting);
+                while (matcher.find()) {
+                    List<Integer> calculation = Arrays.stream(
+                            matcher.group()
+                                    .substring(4, matcher.group().length() - 1)
+                                    .split(","))
+                            .map(Integer::valueOf).toList();
+                    d.part1 += calculation.get(0) * calculation.get(1);
+                    if (enable) {
+                        d.part2 += calculation.get(0) * calculation.get(1);
+                    }
+                }
+            }
         }
         System.out.println(d.part1);
+        System.out.println(d.part2);
     }
 }
