a763eb2bd0175dfd0ff76b1a93be7260e310afc0
Pim van de Streek <git@vandestreek.it>
2024-12-08 22:34:18 +0100

    Completed day 7 part 2

--- src/day7/Day7.java
+++ src/day7/Day7.java
@@ -6,19 +6,27 @@ import java.util.List;
 
 public class Day7 {
     private final String filePath = "input/day7.txt";
-    private int part1;
-    private int part2;
+    private long part1;
+    private long part2;
 
     public static void main(String[] args) {
+        // Start timer
+        long startTime = System.nanoTime();
+
         Day7 d = new Day7();
 
         List<String> input = Util.readFileAsList(d.filePath);
 
-        for (String equation: input) if (d.equationValid(equation)) d.part1++;
-        System.out.println(d.part1);
+        for (String equation: input) d.part2 += (d.equationValid(equation)) ;
+        System.out.println(d.part2);
+
+        // Stop timer
+        long endTime = System.nanoTime();
+        long elapsedTime = endTime - startTime;
+        System.out.println("Uitvoeringstijd tot part 1: " + elapsedTime / 1_000_000 + " milliseconds");
     }
 
-    private boolean equationValid(String equation) {
+    private long equationValid(String equation) {
         long target = Long.parseLong(equation.split(": ")[0]);
         boolean targetReached = false;
 
@@ -30,7 +38,8 @@ public class Day7 {
         if (!values.isEmpty()) {
             targetReached = this.addOrMultiply(values.subList(1,values.size()), answer, target);
         }
-        return targetReached;
+        if (targetReached) return target;
+        return 0;
     }
 
     private boolean addOrMultiply(List<Integer> values, long answer, long target) {
@@ -40,11 +49,15 @@ public class Day7 {
         if (values.size() == 1) {
             if (answer + values.getFirst() == target) return true;
             if (answer * values.getLast() == target) return true;
+            if (Long.parseLong(String.valueOf(answer) + String.valueOf(values.getFirst())) == target) return true;
             return false;
         }
         if (values.size() > 1) {
             if (this.addOrMultiply(values.subList(1, values.size()), answer + values.getFirst(), target)) return true;
             if (this.addOrMultiply(values.subList(1, values.size()), answer * values.getFirst(), target)) return true;
+            if (this.addOrMultiply(values.subList(1, values.size()),
+                    Long.parseLong(String.valueOf(answer) + String.valueOf(values.getFirst())),
+                    target)) return true;
         }
         return false;
     }
